7b1ba0186ce2316b9bfdbf785bd5a2a0adf8187d
cavadasA <antonio.cavadas.gu@gmail.com>
2021-05-05 21:05:41 +0000

    card horizontal alojamientos

--- src/front/js/component/cardAlojamiento.js
+++ src/front/js/component/cardAlojamiento.js
@@ -9,27 +9,33 @@ export const CardAlojamiento = props => {
 	const { actions } = useContext(Context);
 
 	return (
-		<div className="card" style={{ width: 22 + "rem" }}>
-			<div className="card-body">
-				<img
-					src="https://www.autonomosyemprendedor.es/media/autonomosyemprendedor/images/2020/04/30/2020043016011015191.jpg"
-					className="card-img-top"></img>
-				<h5 className="card-title mt-3">{props.title}</h5>
-				<h6 className="card-subtitle mb-2 text-muted">
-					{props.ciudad}, {props.provincia}
-				</h6>
-				<p className="card-text">Huespedes: {props.huespedes}</p>
-				<p className="card-text">Dormitorios: {props.dormitorios}</p>
-				<p className="card-text">Baños: {props.bathrooms}</p>
+		<div className="card" style={{ width: 540 + "px" }}>
+			<div className="row no-gutters">
+				<div className="col-md-6 ">
+					<img
+						src="https://www.autonomosyemprendedor.es/media/autonomosyemprendedor/images/2020/04/30/2020043016011015191.jpg"
+						className="card-img"></img>
+				</div>
+				<div className="col-md-6">
+					<div className="card-body">
+						<h5 className="card-title">{props.title}</h5>
+						<h6 className="card-subtitletext-muted">
+							{props.ciudad}, {props.provincia}
+						</h6>
+						<p className="card-text">Huespedes: {props.huespedes}</p>
+						<p className="card-text">Dormitorios: {props.dormitorios}</p>
+						<p className="card-text">Baños: {props.bathrooms}</p>
 
-				<Link
-					to={{
-						pathname: "/descripcionMisPropiedades/" + props.id,
-						state: props
-					}}
-					className="btn btn-primary">
-					Más información
-				</Link>
+						<Link
+							to={{
+								pathname: "/descripcionMisPropiedades/" + props.id,
+								state: props
+							}}
+							className="btn btn-primary">
+							Más información
+						</Link>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
