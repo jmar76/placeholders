43cbd7c471b37bc1a6017e1d965d87023a337dbf
jmar76 <jmaguero.ing@gmail.com>
2021-06-01 07:24:31 +0000

    actualprueba

--- src/front/js/pages/viajeros.js
+++ src/front/js/pages/viajeros.js
@@ -2,6 +2,9 @@ import React, { Fragment } from "react";
 import "../../styles/viajeros.scss";
 import { Link } from "react-router-dom";
 export const Viajeros = () => {
+    useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
 	return (
 		<Fragment>
 			<div className="container-fluid fondoDePantallaviajero">
